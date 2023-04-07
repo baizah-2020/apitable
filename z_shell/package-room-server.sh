4e2a63027e898ffdb88ab63f7ba4f330ccfdfd9f
abc-office <abc-office@gmail.com>
2023-04-07 17:32:19 +0800

    Dockerfile.room-server 文件中添加 本地缓存copy机制指令

--- z_shell/package-room-server.sh
+++ z_shell/package-room-server.sh
@@ -5,9 +5,10 @@ source ./common_util.sh
 cp ./docker-bake-room-server.hcl ../
 
 cd ../
+cp -r ../room-server-deps ./
 
 make build-docker-room-server
 
 rm -f docker-bake-room-server.hcl
-
+rm -rf room-server-deps
 
