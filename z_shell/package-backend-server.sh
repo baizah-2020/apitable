69a16cbabb3d12cdb21de5c0efa06ac72e043148
abc-office <abc-office@gmail.com>
2023-04-07 15:19:36 +0800

    Dockerfile.backend-server 文件添加本地缓存 gradle 包

--- z_shell/package-backend-server.sh
+++ z_shell/package-backend-server.sh
@@ -5,9 +5,11 @@ source ./common_util.sh
 cp ./docker-bake-backend-server.hcl ../
 
 cd ../
-
+cp -r ../gradle ./
 make build-docker-backend-server
 
 rm -f docker-bake-backend-server.hcl
+
+rm -rf ./gradle
 
 
