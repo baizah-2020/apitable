4fdb6441be414ce79aa3a96a2dd2d3c59db98fee
abc-office <abc-office@gmail.com>
2023-04-08 10:25:17 +0800

    update

--- z_shell/package-room-server.sh
+++ z_shell/package-room-server.sh
@@ -5,10 +5,10 @@ source ./common_util.sh
 cp ./docker-bake-room-server.hcl ../
 
 cd ../
-cp -r ../room-server-deps ./
+#cp -r ../room-server-deps ./
 
 make build-docker-room-server
 
 rm -f docker-bake-room-server.hcl
-rm -rf room-server-deps
+#rm -rf room-server-deps
 
