3708ad0b09db678d08e38e4f7ce3f9a0ab09d909
abc-office <abc-office@gmail.com>
2023-04-09 08:03:55 +0800

    update

--- packages/datasheet/src/pc/components/common/logo/logo.tsx
+++ packages/datasheet/src/pc/components/common/logo/logo.tsx
@@ -103,7 +103,8 @@ export const Logo: React.FC<React.PropsWithChildren<ILogoProps>> = (props) => {
       <span style={{ width: logoSize.logoSize, height: logoSize.logoSize }}>
         {renderLogo()}
       </span>
-      {renderLogoText()}
+      {/*{renderLogoText()}*/}
+      <span style={{fontSize: '30px',color: '#fff',marginLeft: '20px'}}>Smart OA</span>
     </span>
   );
 };
