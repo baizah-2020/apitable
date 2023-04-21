0eb99a5e2a601fd6809421fe388bf3e2603863fd
abc-office <abc-office@gmail.com>
2023-04-21 20:40:24 +0800

    update

--- z_shell/seds/sed-1.4.sh
+++ z_shell/seds/sed-1.4.sh
@@ -6,9 +6,9 @@ source ../common_util.sh
 cd ../../
 
 # all
-sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g' packages/i18n-lang/src/config/strings.auto.json
-sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g'  packages/i18n-lang/src/config/strings.edition.auto.json
-sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g'  packages/i18n-lang/src/config/strings.json
+#sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g' packages/i18n-lang/src/config/strings.auto.json
+#sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g'  packages/i18n-lang/src/config/strings.edition.auto.json
+#sed -i 's#apitable.com#darcy.zhang.2023@gmail.com#g'  packages/i18n-lang/src/config/strings.json
 
 
 sed -i 's#APITable#Smart OA#g' packages/i18n-lang/src/config/strings.auto.json
